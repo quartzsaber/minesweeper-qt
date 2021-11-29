9d1d563571b45680207d1e506ccbb4e36b292cec
이예원 <81704418+scarletYewon@users.noreply.github.com>
2021-11-29 10:48:11 +0900

    gameaction 구현

--- gameaction.py
+++ gameaction.py
@@ -1,13 +1,13 @@
 # 클라이언트가 게임에서 행한 동작을 dict 형식으로 바꿈
 class GameAction:
-    def __init__(self):
-        pass
-
     def openCell(self, x, y):
-        pass
+        dic = {'action': 'openCell', 'x': x, 'y': y}
+        return dic
 
     def openCellAdjacent(self, x, y):
-        pass
+        dic = {'action': 'openCellAdjacent', 'x': x, 'y': y}
+        return dic
 
     def cycleCellImage(self, x, y):
-        pass
+        dic = {'action': 'cycleCellImage', 'x': x, 'y': y}
+        return dic
